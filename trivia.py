10ace4a696980bb9c374f572a57432c14cf2fc82
megops <megan.fcoyle@gmail.com>
2024-12-26 13:55:42 -0500

    added main function where user answers 5 questions and outputs a score

--- trivia.py
+++ trivia.py
@@ -2,25 +2,43 @@ import csv
 import random #inclusive
 
 def main():
-    values = get_csv_file('data.csv') 
-    triviaQuestion=get_trivia_questions(values)
-    print(triviaQuestion)
+    values = get_csv_file('data.csv')
+    total_score = 0
+    max_num = 5
+    print("Welcome to Dota CL Triva!")
+    for _ in range(5):
+        triviaQuestion=get_trivia_question(values)
+        total_score += play_game(triviaQuestion)
+    print(f"Your total score is {total_score}/{max_num}")
+    print("Thanks for playing!")
+
 
 
 #populate dictionary with data.csv file
 def get_csv_file(csvfile):
     with open(csvfile, newline='') as csvfile:
         reader = csv.reader(csvfile)
-        Values = {}
+        values = {}
         for row in reader:
-            key = row[0]
-            value = row[1]
-            Values[key] = value
-    return Values
+            key = row[0].strip()  # strip whitespace from the key
+            value = row[1].strip()  # strip whitespace from the value
+            values[key] = value
+    return values
 
-def get_trivia_questions(dict):
+def get_trivia_question(dict):
     key = random.choice(list(dict.keys()))  # randomly select a key
     return key, dict[key] 
 
+def play_game(triviaQuestion):
+    hero = input(f"Who said this dota2 quote: \"{triviaQuestion[1]}\"? ") 
 
+    score = 0
+    if hero.lower().strip() == triviaQuestion[0].lower().strip():  # Compare ignoring case and leading/trailing spaces
+        score += 1
+        print("Outcome: Correct")
+    else:
+        print(f"Outcome: Wrong")
+        print(f"Correct answer is {triviaQuestion[0]}")
+    
+    return score
 main()
